68b9fe0bc09078327a46156e794cd66e9fca582a
dmclink <135415819+dmclink@users.noreply.github.com>
2025-09-29 16:22:59 +0800

    build: setup webpack and eslint

--- setup/index.js
+++
@@ -1,8 +0,0 @@
-export { setupWebpack } from './webpack.js';
-export { installDependencies } from './packagemanager.js';
-export { updatePackageProperty } from './packagejson.js';
-export {
-	removeFromGitTracking,
-	deleteFilesAndDirs,
-	getProjectName,
-} from './files.js';
---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+	entry: './src/js/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/html/template.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+};
