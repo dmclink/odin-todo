9272d9e664d65306729ac13fbff56b5f4c961ef9
dmclink <135415819+dmclink@users.noreply.github.com>
2025-09-29 21:36:18 +0800

    docs: jsdocs to project methods

--- src/js/projects.js
+++ src/js/projects.js
@@ -7,6 +7,10 @@ class Project {
     this.#todos = [];
   }
 
+  /** Adds new todo to this project's list
+   * 
+   * @param {ToDo} newTodo - the new todo to add to the list
+   */
   add(newTodo) {
     if (!(newTodo instanceof ToDo)) {
       throw new TypeError('projects only accepts ToDo items')
@@ -14,6 +18,10 @@ class Project {
     this.#todos.push(newTodo);
   }
 
+  /** Removes the todo from this project's list that has a matching id with input
+   * 
+   * @param {string} id - the id of the todo we want to delete 
+   */
   remove(id) {
     const idx = this.#todos.find((todo) => todo.id === id);
     if (!idx) {
