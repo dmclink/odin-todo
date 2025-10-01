fc333d6ab1b47c1576fb27d7bc8751afb443502d
dmclink <135415819+dmclink@users.noreply.github.com>
2025-10-01 19:47:24 +0800

    fix: incorrect import statement causing build fail

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,1 @@
-import ToDo from './todo.js';
-
-const todo1 = new ToDo(
-	'new todo',
-	'new description',
-	new Date(Date.now()),
-	'low'
-);
-
-console.log(todo1.read());
+import ProjectManager from './projectmanager.js';
--- src/js/project.js
+++ src/js/project.js
@@ -1,45 +1,57 @@
-import ToDo from ('./todo.js')
+import ToDo from './todo.js';
 
 export default class Project {
-  #name;
-  #todos;
-  #id;
+	#name;
+	#todos;
+	#id;
 
-  constructor(name) {
-    this.#name = name;
-    this.#todos = [];
-    this.#id = crypto.randomUUID();
-  }
+	constructor(name) {
+		this.#name = name;
+		this.#todos = [];
+		this.#id = crypto.randomUUID();
+	}
 
-  /** Adds new todo to this project's list
-   * 
-   * @param {ToDo} newTodo - the new todo to add to the list
-   */
-  add(newTodo) {
-    if (!(newTodo instanceof ToDo)) {
-      throw new TypeError('projects only accepts ToDo items')
-    }
-    this.#todos.push(newTodo);
-  }
+	/** Adds new todo to this project's list
+	 *
+	 * @param {ToDo} newTodo - the new todo to add to the list
+	 */
+	add(newTodo) {
+		if (!(newTodo instanceof ToDo)) {
+			throw new TypeError('projects only accepts ToDo items');
+		}
+		this.#todos.push(newTodo);
+	}
 
-  /** Removes the todo from this project's list that has a matching id with input
-   * 
-   * @param {string} id - the id of the todo we want to delete 
-   */
-  remove(id) {
-    const idx = this.#todos.find((todo) => todo.id === id);
-    if (!idx) {
-      throw new Error('non existing id passed to remove');
-    }
+	/** Removes the todo from this project's list that has a matching id with input
+	 *
+	 * @param {string} id - the id of the todo we want to delete
+	 */
+	remove(id) {
+		const idx = this.#todos.find((todo) => todo.id === id);
+		if (!idx) {
+			throw new Error('non existing id passed to remove');
+		}
 
-    this.#todos.splice(idx, 1);
-  }
+		this.#todos.splice(idx, 1);
+	}
 
-  get id() {
-    return this.#id;
-  }
+	get id() {
+		return this.#id;
+	}
 
-  getTodos() {
-    return [...this.#todos];
-  }
+	get name() {
+		return this.#name;
+	}
+
+	set name(newName) {
+		if (!newName) {
+			throw new Error('cannot set to empty name');
+		}
+
+		this.#name = newName;
+	}
+
+	getTodos() {
+		return [...this.#todos];
+	}
 }
