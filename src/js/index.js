ecbf65a69572b82e1afbee6401aebeacd6acb83a
dmclink <135415819+dmclink@users.noreply.github.com>
2025-10-06 15:16:00 +0800

    feat: add html skeleton

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,1 @@
 import ProjectManager from './projectmanager.js';
-import ToDo from './projectmanager.js';
-
-window.ProjectManager = ProjectManager;
-
-window.pm = new ProjectManager();
-
-console.log('hello console');
-console.log(pm);
