5ee966cc12d75b74af65fbdbbe2cfbb3359126c7
dmclink <135415819+dmclink@users.noreply.github.com>
2025-11-02 10:25:17 +0700

    feat: style filter controls

--- src/js/displaycontroller.js
+++ src/js/displaycontroller.js
@@ -9,30 +9,105 @@ export default class DisplayController {
 	#htmlEl = document.querySelector('html');
 	#newToDoModal = document.querySelector('#new-todo');
 	#newToDoForm = document.querySelector('#new-todo__form');
-	#selectedProject;
+	#headerTitle = document.querySelector('#header__title');
+	#headerCount = document.querySelector('#header__active-todos');
+	#statusFilterBtns = document.querySelectorAll('.filter__button');
+	#selectedStatusFilter = 'active';
 
+	// 'All Projects' has an empty string for id, this will be the default selection on loading
+	#selectedProject = '';
+
+	bindEvents() {
+		// bind event for dark mode toggle button
+		document
+			.querySelector('#header__dark-toggle')
+			.addEventListener('click', () => {
+				this.toggleDarkMode();
+			});
+
+		// bind event to open modal
+		document.querySelector('#header__add-btn').addEventListener('click', () => {
+			this.showModal();
+		});
+
+		// bind event to close modal on cancel button click
+		document
+			.querySelector('#new-todo__cancel')
+			.addEventListener('click', () => {
+				this.closeModal();
+			});
+
+		// bind event for selecting status filters
+		this.#statusFilterBtns.forEach((btn) => {
+			console.log(btn, 'clicked');
+			btn.addEventListener('click', () => {
+				this.selectStatusFilter(btn);
+			});
+		});
+	}
+
+	selectStatusFilter(btn) {
+		this.#statusFilterBtns.forEach((btn) => {
+			btn.removeAttribute('aria-selected');
+		});
+
+		btn.setAttribute('aria-selected', 'true');
+	}
+
+	selectedStatusFilter() {
+		return this.#selectedStatusFilter;
+	}
+
+	// brings up the add new todo modal. called when "+ Add Todo" button clicked
 	showModal() {
 		this.#newToDoModal.showModal();
 	}
 
+	// closes the add new todo modal and resets all form fields
 	closeModal() {
 		this.#newToDoForm.reset();
 		this.#newToDoModal.close();
 	}
 
+	// returns the form data from the add new todo modal
 	getFormData() {
 		return new FormData(this.#newToDoForm);
-		// TODO:
+	}
+
+	// updates header title elements with selected project's name and count
+	updateHeader() {
+		const { name, count } = this.getSelectedNameCount();
+		this.#headerTitle.textContent = name;
+		this.#headerCount.textContent = count;
+	}
+
+	// returns the name and count of the currently selected project
+	// based on the data written into the projects list. the projects list
+	// fully refreshed whenever changes are made so the data can be considered up to date
+	getSelectedNameCount() {
+		const id = this.#selectedProject;
+		const projectBtns = this.#projectsList.querySelectorAll('.projects__btn');
+
+		const selectedBtn = Array.from(projectBtns).find(
+			(btn) => btn.getAttribute('data-id') === id
+		);
+
+		const name = selectedBtn.getAttribute('data-name');
+		const count = selectedBtn.getAttribute('data-count');
+
+		return { name, count };
+	}
+
+	// returns the id of the currently selected project
+	selectedProject() {
+		return this.#selectedProject;
 	}
 
 	selectProject(id) {
 		this.#selectedProject = id;
 		// TODO: update highlight class on projects list
-		// TODO: update name and count in header
-	}
 
-	selectedProject() {
-		return this.#selectedProject;
+		this.updateHeader();
 	}
 
 	toggleDarkMode() {
@@ -57,10 +132,18 @@ export default class DisplayController {
 			newProjectEl.querySelector('.projects__project-name').textContent =
 				project.name;
 
-			newProjectEl.querySelector('.projects__todo-count').textContent =
-				project.count;
+			const todoCount = newProjectEl.querySelector('.projects__todo-count');
 
-			newProjectEl['data-id'] = project.id;
+			todoCount.textContent = project.count;
+
+			const btn = newProjectEl.querySelector('.projects__btn');
+			btn.setAttribute('data-id', project.id || '');
+			btn.setAttribute('data-name', project.name || '');
+			btn.setAttribute('data-count', project.count || '0');
+
+			btn.addEventListener('click', () => {
+				this.selectProject(btn.getAttribute('data-id'));
+			});
 
 			this.#projectsList.appendChild(newProjectEl);
 		});
