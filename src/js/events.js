903d248ce5e4fb28be952d055d85caac4220747b
dmclink <135415819+dmclink@users.noreply.github.com>
2025-11-02 10:45:38 +0700

    feat: add event emitter

---
+++ src/js/events.js
@@ -0,0 +1,26 @@
+export default class EventEmitter {
+	#events;
+
+	constructor() {
+		this.#events = {};
+	}
+
+	on(name, fn) {
+		this.#events[name] = this.#events[name] || [];
+		this.#events[name].push(fn);
+	}
+
+	off(name, rmFn) {
+		if (this.#events[name]) {
+			this.#events[name] = this.#events[name].filter((fn) => fn !== rmFn);
+		}
+	}
+
+	emit(name, ...args) {
+		if (this.#events[name]) {
+			this.#events[name].forEach((fn) => {
+				fn(...args);
+			});
+		}
+	}
+}
